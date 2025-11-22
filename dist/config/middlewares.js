5066796b483551573fbdde900c67f69173677a15
technology2025936 <technology@insideoutconsult.com>
2025-11-22 15:25:33 +0530

    url

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
             origin: [
                 'https://vulcan-sky.vercel.app',
                 'http://localhost:8080',
-                'https://www.flyvulcan.co.za/'
+                'https://flyvulcan.co.za/'
             ],
             methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
             headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
